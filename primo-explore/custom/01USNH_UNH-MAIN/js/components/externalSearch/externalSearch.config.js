7c4de2e877c44b1cea643b350f27b480431dfd6d
Jay L. Colbert <jay.colbert@unh.edu>
2021-04-21 13:52:45 +0000

    edited Google Scholar link to use the proxy

--- primo-explore/custom/01USNH_UNH-MAIN/js/components/externalSearch/externalSearch.config.js
+++ primo-explore/custom/01USNH_UNH-MAIN/js/components/externalSearch/externalSearch.config.js
@@ -31,7 +31,7 @@ const ExternalSearchConfig = [
   },
   {
     name: "Google Scholar",
-    url: "https://scholar.google.com/scholar?q=",
+    url: "https://unh.idm.oclc.org/login?qurl=https%3A//scholar.google.com/scholar?q=",
     img:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png",
     alt: "Google Scholar Logo",
